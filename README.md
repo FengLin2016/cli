fbdc223473ea3766bbb522a2a67e87fc5b2437d0
fenglin <786549791@qq.com>
2019-02-28 16:33:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# static-cli
+
+```
+// 开启静态资源开发
+npm i dgg-static-cli -g 
+
+dgg-static-cli stylus<可换成你熟悉的> myobject<项目名称>
+
+cd myobject // 进入项目文件夹
+
+cnpm i // 安装依赖
+
+gulp //启动gulp
+
+```"# cli" 
---
+++ index.js
@@ -0,0 +1,28 @@
+#! node
+
+const clone = require('git-clone')
+const program = require('commander')
+const shell = require('shelljs');
+const log = require('tracer').colorConsole()
+
+
+program
+    .version('1.0.0')
+    .description('静态资源工程的cli')
+program
+    .command('* <tpl> <project>')
+    .action(function(tpl, project) {
+        log.info('目前static-cli支持各种css语言定制：less、sass、stylus')
+        log.info('使用例子：dgg-static-cli stylus myproject')
+        if (tpl && project) {
+            let pwd = shell.pwd()
+            log.info(`正在拉取模板代码，下载位置：${pwd}/${project}/ ...`)
+            clone(`https://github.com/FengLin2016/static-cli.git`, pwd + `/${project}`, null, function() {
+                shell.rm('-rf', pwd + `/${project}/.git`)
+                log.info('模板工程建立完成')
+            })
+        } else {
+            log.error('正确命令例子：dgg-static-cli stylus myproject')
+        }
+    })
+program.parse(process.argv)
